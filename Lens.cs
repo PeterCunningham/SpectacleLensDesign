3debfe3effee09deba973360a975b2d53f5099ae
Peter Cunningham <peter.cunningham@serafimltd.com>
2018-11-13 17:17:20 +0000

    Initial object-oriented framework

---
+++ Lens.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectacleLensDesign
+{
+    class Lens
+    {
+        private int n_i, n_j;
+        double[,] Front_z;
+        double[,] Rear_z;
+        public double[,] Power;
+        double RefractiveIndex;
+        
+        void SetCartesianArraysSize(int Num_I, int Num_J)
+        {
+            n_i = Num_I;
+            n_j = Num_J;
+
+            Front_z = new double[n_i, n_j];
+            Rear_z = new double[n_i, n_j];
+            Power = new double[n_i, n_j];
+        }
+        void CalculatePower() { }
+
+
+    }
+
+
+}
---
+++ Prescription.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectacleLensDesign
+{
+    class Prescription
+    {
+        Lens OptimisedLens;
+
+        void CalculateOptimisedLens()
+        {
+
+
+        }
+
+        double LensQuality(Lens LensToEvaluate)
+        {
+            LensToEvaluate.Power[1, 1] = 0.0; /* dummy code */
+            return 0.0;
+        }
+    }
+}
